fc6cc4253a5165fdae3a67d56a1909a51a213376
AlexSpiesi <alex.spiesmeier@gmail.com>
2025-05-24 11:42:08 +0200

    created a default user

--- db.js
+++ db.js
@@ -1,8 +1,9 @@
 const sqlite3 = require('sqlite3').verbose();
+const bcrypt = require('bcrypt');
 
 const db = new sqlite3.Database(':memory:');
 
-db.serialize(() => {
+db.serialize(async() => {
     db.run(`CREATE TABLE users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         email TEXT UNIQUE NOT NULL,
@@ -15,12 +16,14 @@ db.serialize(() => {
         bmi_category TEXT
     )`);
     //Real = float in SQLite
+
+    const hashedPassword = await bcrypt.hash('password1', 10);
         
     const stmt = db.prepare(`
-        INSERT INTO users (email, password, age, gender)
-         VALUES (?,?,?,?)
+        INSERT INTO users (email, password, age, gender, weight, height)
+         VALUES (?,?,?,?,?,?)
         `);
-    stmt.run(`Barbara@gmail.com`,`FatShadow`,30,`female`);
+    stmt.run(`bob@test.at`,hashedPassword,30,`male`, 80, 180);
     stmt.finalize();
 
 });
